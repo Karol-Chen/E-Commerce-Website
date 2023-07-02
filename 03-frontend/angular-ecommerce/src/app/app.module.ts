ff65500de75258a4b2eabf03ebb3dc73836436cb
Daixuan Chen <DaixuanChenShaw@gmail.com>
2023-07-01 23:39:43 -0400

    add pagination and cart feature

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -6,14 +6,38 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import{HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
+import { Routes,RouterModule } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+
+const routes: Routes=[
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path:'search/:keyword',component: ProductListComponent},
+  {path: 'category/:id/:name', component: ProductListComponent},
+  {path:'category',component:ProductListComponent},
+  {path:'products',component:ProductListComponent},
+  {path:'',redirectTo:'/products',pathMatch:'full'},
+  {path:'**',redirectTo:'/products',pathMatch:'full'}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent,
+    CartStatusComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from '../common/product';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +11,78 @@ import { map } from 'rxjs/operators';
 export class ProductService {
 
   private baseUrl = "http://localhost:8080/api/products";
+  private categoryUrl="http://localhost:8080/api/product-category"
 
   constructor(
     private httpClient: HttpClient
   ) { }
 
-  getProductList():Observable<Product[]>{
-    let object=this.httpClient.get<GetResponse>(this.baseUrl).pipe(map(response => response._embedded.products));
-    debugger;
-    // console.log(object);
-    return object;
+  getProductList(theCategoryId: number):Observable<Product[]>{
+    //build URL based on category id
+    const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  getProductListPaginate(thePage: number, 
+                         thePageSize: number,
+                         theCategoryId: number):Observable<GetResponseProducts>{
+
+    //build URL based on category id, page and size
+    const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`+`&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
+  searchProducts(theKeyword: string) : Observable<Product[]>{
+
+    //need to build URL based on the keyword
+    const searchUrl=`${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  searchProductPaginate(thePage:number,
+                        thePageSize: number,
+                        theKeyword: string): Observable<GetResponseProducts>{
+      
+      const searchUrl=`${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                      + `&page=${thePage}&size=${thePageSize}`;
+
+      return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
+  getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(map(response => response._embedded.products));
+  }
+
+  getProduct(theProductId:number):Observable<Product>{
+
+    //need to build the Url based on the product id
+    const productUrl=`${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
+
+  getProductCategories():Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(map(response => response._embedded.productCategory));
   }
 }
 
-interface GetResponse{
+interface GetResponseProducts{
   _embedded: {
     products:Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
+  }
+}
+
+interface GetResponseProductCategory{
+  _embedded: {
+    productCategory:ProductCategory[];
   }
 }
